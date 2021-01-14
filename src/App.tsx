99fc1f29696a60e4b00c938903d2823ff14c6acd
Max Panasenko <maxim.p.jsdev@gmail.com>
2021-01-14 13:54:53 +0200

    add linter setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>Hello world!</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>Hello world!</div>
+        </div>
+    );
 }
 
 export default App;
